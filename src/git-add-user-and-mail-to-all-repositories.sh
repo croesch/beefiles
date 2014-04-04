39c2efa0ab9f32bfd724c29a5745df32714ac08c
Christian Rösch <Christian.Roesch@icongmbh.de>
2014-04-04 10:40:32 +0200

    Improve output of script.

--- src/git-add-user-and-mail-to-all-repositories.sh
+++ src/git-add-user-and-mail-to-all-repositories.sh
@@ -2,10 +2,12 @@
 
 for repo in `find / -type d -name .git 2> /dev/null`
 do
+  echo -n "Adding user to ${repo}.. "
   repoConfig="${repo}/config"
   if grep "\[user\]" "${repoConfig}" > /dev/null; then
-    echo "${repoonfig}" already contains user!!!
+    echo "already contains user!!!"
   else
     cat ~/.gitconfig_user >> "${repoConfig}"
+    echo "done."
   fi
 done
