b14e9248a4544b857ba1a8f332d9ff3266c9fae3
Christian Rösch <christianroesch@gmx.net>
2014-08-11 00:37:20 +0200

    Read  from mvn command if not set.

--- src/maven-repository-houskeeping.sh
+++ src/maven-repository-houskeeping.sh
@@ -20,6 +20,13 @@ fi
 
 date >> ${LOGFILE}
 echo "------------------------------------------------------------------------" >> ${LOGFILE}
+
+if [ -z "${M2_REPO}" ]
+then
+  M2_REPO=$(mvn help:evaluate -Dexpression=settings.localRepository | grep -v '[INFO]')
+  echo "M2_REPO was not set, set it to '$M2_REPO'" >> ${LOGFILE}
+fi
+
 find "${M2_REPO}" -name '*jar' -atime +${AGE} -exec rm -rfv {} \; >> ${LOGFILE}
 find "${M2_REPO}" -name '*swf' -atime +${AGE} -exec rm -rfv {} \; >> ${LOGFILE}
 find "${M2_REPO}" -name '*swc' -atime +${AGE} -exec rm -rfv {} \; >> ${LOGFILE}
