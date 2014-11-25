4b90d1b9430595b84d747807f3a17ba3d49309eb
Christian Rösch <christianroesch@gmx.net>
2014-11-25 21:10:20 +0100

    Add script to move all files from camera to dedicated folders.

---
+++ src/collect-camera-files.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]
+then
+  echo "Wrong usage.."
+  exit 1
+else
+  CAMERA="${1}"
+fi
+
+find "${CAMERA}/DCIM/" -type f -iregex .*\.jpe?g |
+while read file
+do
+  echo -n "${file}.."
+  datetaken=$(exiftool -s3 '-CreateDate' -d '%Y-%m-%d' "${file}")
+  yeartaken=$(exiftool -s3 '-CreateDate' -d '%Y' "${file}")
+  filename=$(basename "${file}")
+  target="${HOME}/Pictures/${yeartaken}/${datetaken}/"
+  mkdir -p "${target}"
+  rsync -a --remove-source-files "${file}" "${target}"
+  jpegoptim -q "${target}${filename}"
+  echo "done."
+done
+
+find "${CAMERA}/MP_ROOT/" -type f -iregex .*\.mp4 |
+while read file
+do
+  echo -n "${file}.."
+  datetaken=$(exiftool -s3 '-CreateDate' -d '%Y-%m-%d' "${file}")
+  yeartaken=$(exiftool -s3 '-CreateDate' -d '%Y' "${file}")
+  filename=$(basename "${file}")
+  target="${HOME}/Pictures/${yeartaken}/${datetaken}/videos/"
+  mkdir -p "${target}"
+  rsync -a --remove-source-files "${file}" "${target}"
+  echo "done."
+done
