993be9066c5aba9e65e93d528f759ac8e2ca0673
Christian Rösch <christianroesch@gmx.net>
2014-11-25 21:20:04 +0100

    Improve output and delete thm-files.

--- src/collect-camera-files.sh
+++ src/collect-camera-files.sh
@@ -11,13 +11,15 @@ fi
 find "${CAMERA}/DCIM/" -type f -iregex .*\.jpe?g |
 while read file
 do
-  echo -n "${file}.."
+  echo -n "${file} "
   datetaken=$(exiftool -s3 '-CreateDate' -d '%Y-%m-%d' "${file}")
   yeartaken=$(exiftool -s3 '-CreateDate' -d '%Y' "${file}")
   filename=$(basename "${file}")
   target="${HOME}/Pictures/${yeartaken}/${datetaken}/"
   mkdir -p "${target}"
+  echo -n "."
   rsync -a --remove-source-files "${file}" "${target}"
+  echo -n "."
   jpegoptim -q "${target}${filename}"
   echo "done."
 done
@@ -25,12 +27,22 @@ done
 find "${CAMERA}/MP_ROOT/" -type f -iregex .*\.mp4 |
 while read file
 do
-  echo -n "${file}.."
+  echo -n "${file} "
   datetaken=$(exiftool -s3 '-CreateDate' -d '%Y-%m-%d' "${file}")
   yeartaken=$(exiftool -s3 '-CreateDate' -d '%Y' "${file}")
   filename=$(basename "${file}")
   target="${HOME}/Pictures/${yeartaken}/${datetaken}/videos/"
   mkdir -p "${target}"
+  echo -n "."
   rsync -a --remove-source-files "${file}" "${target}"
   echo "done."
 done
+
+find "${CAMERA}/MP_ROOT/" -type f -iregex .*\.thm |
+while read file
+do
+  echo -n "${file} "
+  rm "${file}"
+  echo "removed."
+done
+
