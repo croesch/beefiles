c4a47c95d78ecab744c44e26d4b1d70ecaad8617
Christian Rösch <Christian.Roesch@icongmbh.de>
2016-10-10 14:00:31 +0200

    Remove manual log rotation and increase max file age.

--- src/maven-repository-houskeeping.sh
+++ src/maven-repository-houskeeping.sh
@@ -2,20 +2,12 @@
 
 LOGNAME=maven-housekeeping.log
 LOGFILE="/var/log/${LOGNAME}"
-TMPLOG="/tmp/${LOGNAME}"
-MAX_LOG_SIZE=1000000
 
-AGE=100
+AGE=365
 
 if [ "$#" -eq 1 ]
 then
   LOGFILE="${1}"
-fi
-
-if [ -f "${LOGFILE}" ]
-then
-  mv ${LOGFILE} ${TMPLOG}
-  tail -n ${MAX_LOG_SIZE} ${TMPLOG} > ${LOGFILE}
 fi
 
 date >> ${LOGFILE}
